49e9a3b5eeb471b2175e72293d6ead318f24d32f
Rod Leviton <rod@rodlevtion.com>
2016-10-06 09:20:38 +1100

    add http request framework

--- test/basic/test.js
+++ test/basic/test.js
@@ -1,170 +1,49 @@
-var should = require('should');
+var chai = require('chai');
+var expect = chai.expect;
+var request = require('superagent');
+require('superagent-proxy')(request);
+var proxy = process.env.http_proxy;
 
-describe('Master Test Suite', function () {
-  describe('Test Suite - Basic', function () {
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
+describe('API Test Suite', function() {
+  describe('Get Posts', function() {
+    it('should return a status of 200', function (done) {
+      request.get('http://jsonplaceholder.typicode.com/posts')
+      // .proxy(proxy)
+        .end(function (err, res) {
+          expect(res.status).to.equal(200);
+          done();
+        });
     });
-    it('failing test', function (done) {
-      false.should.be.ok;
-      done();
+
+    it('should return a valid payload', function (done) {
+      request.get('http://jsonplaceholder.typicode.com/posts')
+      // .proxy(proxy)
+        .end(function (err, res) {
+          expect(res).to.exist;
+          done();
+        });
     });
   });
 
-  describe('Test Suite - Nested Suites', function () {
-    describe('Nested Test Suite', function () {
-      it('passing test', function (done) {
-        true.should.be.ok;
-        done();
-      });
-    });
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-    describe('Nested Test Suite', function () {
-      it('passing test', function (done) {
-        true.should.be.ok;
-        done();
-      });
-    });
-    it('failing test', function (done) {
-      false.should.be.ok;
-      done();
-    });
-  });
-
-  describe('Test Suite - Nested Suites Failing Before', function () {
-    before('nested failing before', function() {
-      console.log(a);
-    });
-    describe('Nested Test Suite', function () {
-      it('passing test', function (done) {
-        true.should.be.ok;
-        done();
-      });
-    });
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-  });
-
-  describe('Test Suite - Failed After', function () {
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-    xit('pending test', function (done) {
-      done();
-    });
-    it('failing test', function (done) {
-      false.should.be.ok;
-      done();
-    });
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-    xit('pending test', function (done) {
-      done();
-    });
-    after('failing after hook', function () {
-      console.log('a');
-    });
-  });
-
-  describe('Test Suite - Pass', function () {
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-  });
-
-  describe('Test Suite - Fail', function () {
-    it('failing test', function (done) {
-      false.should.be.ok;
-      done();
-    });
-  });
-
-  describe('Test Suite - Pend', function () {
-    xit('pending test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-  });
-
-  describe('Test Suite - Failed Before', function () {
-    before('failing before hook', function () {
-      console.log(a);
-    });
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
+  describe('Get User', function() {
+    it('should return a status of 200', function (done) {
+      request.get('http://jsonplaceholder.typicode.com/posts?userId=1')
+      // .proxy(proxy)
+        .end(function (err, res) {
+          expect(res).to.exist;
+          expect(res.status).to.equal(200);
+          console.log(res);
+          done();
+        });
     });
 
-    it('failing test', function (done) {
-      false.should.be.ok;
-      done();
-    });
-  });
-
-  describe('Test Suite - Failed Before and After', function () {
-    before('failing before hook', function () {
-      console.log(a);
-    });
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-
-    it('failing test', function (done) {
-      false.should.be.ok;
-      done();
-    });
-    after('failing after hook', function () {
-      console.log(a);
-    });
-  });
-
-  describe('Test Suite - Failed Before Each', function () {
-    beforeEach('failing beforeEach hook', function () {
-      console.log(a);
-    });
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-
-    it('failing test', function (done) {
-      false.should.be.ok;
-      done();
-    });
-  });
-
-  describe('Test Suite - Failed After Each', function () {
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-    xit('pending test', function (done) {
-      done();
-    });
-    it('failing test', function (done) {
-      false.should.be.ok;
-      done();
-    });
-    it('passing test', function (done) {
-      true.should.be.ok;
-      done();
-    });
-    xit('pending test', function (done) {
-      done();
-    });
-    afterEach('failing afterEach hook', function () {
-      console.log('a');
+    it('should return a valid payload', function (done) {
+      request.get('http://jsonplaceholder.typicode.com/posts?userId=1')
+      // .proxy(proxy)
+        .end(function (err, res) {
+          expect(res).to.exist;
+          done();
+        });
     });
   });
 });
